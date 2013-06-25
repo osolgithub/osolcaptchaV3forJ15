7c405c51a8fe9a349f7ca7558f512da194f599a2
Sreekanth Dayanand <osolgithub@outsource-online.net>
2013-06-25 19:34:30 +0530

    updated coreforms.php for joomla 1.5

--- osolCaptcha/coreForms.php
+++ osolCaptcha/coreForms.php
@@ -4,30 +4,34 @@ if($isEnabledForForms['enableForContactUs'])
 			{
 				$this->enabledForms['Core Joomla Contact Us']  = array('requestVars' => 'option=com_contact&view=contact|contact.submit',
 																		 'formId' => "emailForm",//if there is no name use formName
-																		 'tagToPlaceCaptchaBefore' =>'<button type="submit" id="buttonValidate" class="button validate">',
-																		 'verifyOnVars' =>'option=com_contact&task=contact.submit',
+																		 'tagToPlaceCaptchaBefore' =>'<button class="button validate" type="submit">',
+																		 'verifyOnVars' =>'option=com_contact&task=submit',
 																		 //'redirectOnfailure' =>JURI::base() ,//JFactory::getURI()->toString(),
 																		 'skipAJAXVerification' => false,
 																		 'onCaptchaFailSetVars' =>'view=contact&task='
 																		);
 			}
+			//echo "<pre>".print_r($_REQUEST,true)."</pre>";
 			if($isEnabledForForms['enableForRegistration'])
 			{
-				$this->enabledForms['Core Joomla Registration']  = array('requestVars' => 'option=com_users&view=registration',
-																 'formId' => "member-registration",//if there is no name use formName 
-																 'tagToPlaceCaptchaBefore' =>'<button type="submit" class="validate">',
-																 'verifyOnVars' =>'option=com_users&task=registration.register',
+				$this->enabledForms['Core Joomla Registration']  = array('requestVars' => array('option=com_user&view=register',
+																								'option=com_user&task=register_save'
+																								),
+																 'formId' => "josForm",//if there is no name use formName 
+																 'tagToPlaceCaptchaBefore' =>'<button class="button validate" type="submit">',
+																 'verifyOnVars' =>'option=com_user&task=register_save',
 																 //'redirectOnfailure' =>JFactory::getURI()->toString(),
 																 'skipAJAXVerification' => false,
-																 'onCaptchaFailSetVars' =>'view=registration&task='
+																// 'onCaptchaFailSetVars' =>'view=register&task=&view=register'
 																);
 			}
 			if($isEnabledForForms['enableForRemind'])
 			{
-				$this->enabledForms['Core Joomla remind username'] = array('requestVars' => 'option=com_users&view=remind',
-																 'formId' => "user-registration",//if there is no name use formName 
+				$this->enabledForms['Core Joomla remind username'] = array('requestVars' => 'option=com_user&view=remind',
+																 //'formId' => "user-registration",//if there is no name use formName 
+																 'formaction_regExp' => $this->createFormActionRegexp('?option=com_user&task=remindusername'),
 																 'tagToPlaceCaptchaBefore' =>'<button type="submit" class="validate">',
-																 'verifyOnVars' =>'option=com_users&task=remind.remind',
+																 'verifyOnVars' =>'option=com_user&task=remindusername',
 																 //'redirectOnfailure' =>JFactory::getURI()->toString(),
 																 'skipAJAXVerification' => false,
 																 'onCaptchaFailSetVars' =>'view=remind&task='
@@ -35,11 +39,12 @@ if($isEnabledForForms['enableForContactUs'])
 			}
 			if($isEnabledForForms['enableForReset'])
 			{
-				$this->enabledForms['Core Joomla reset password'] = array('requestVars' => 'option=com_users&view=reset',
-																 'formId' => "user-registration",//if there is no name use formName 
+				//echo  JRoute::_( 'index.php?option=com_user&task=requestreset' )."<br />";
+				$this->enabledForms['Core Joomla reset password'] = array('requestVars' => 'option=com_user&view=reset',
+																 'formaction_regExp' => $this->createFormActionRegexp( '?option=com_user&task=requestreset' ),//'task=requestreset',
 																 'tagToPlaceCaptchaBefore' =>'<button type="submit" class="validate">',
-																 'verifyOnVars' =>'option=com_users&task=reset.request',
-																 //'redirectOnfailure' =>JFactory::getURI()->toString(),
+																 'verifyOnVars' =>'option=com_user&task=requestreset',
+																 'redirectOnfailure' =>JURI::current().'?option=com_user&view=reset&essp_err=check_failed',//JFactory::getURI()->toString(),
 																 'skipAJAXVerification' => false,
 																 'onCaptchaFailSetVars' =>'view=reset&task='
 																);
@@ -47,26 +52,26 @@ if($isEnabledForForms['enableForContactUs'])
 			if($isEnabledForForms['enableForComLogin'] && 0)
 			{
 				$this->enabledForms['Core Joomla login Module'] = array('requestVars' => '*',
-																		 'formId' => "login-form",//if there is no name use formName
-																		 'ignore_condition' => 'task=user.logout',
+																		 'formId' => "form-login",//if there is no name use formName
+																		 'ignore_condition' => 'task=logout',
 																		 'tagToPlaceCaptchaBefore' =>'<input type="submit"',
-																		 'verifyOnVars' =>'option=com_users&task=user.login',
-																		 'redirectOnfailure' =>JURI::base()."index.php/login" ,//JFactory::getURI()->toString(),
+																		 'verifyOnVars' =>'option=com_user&task=login',
+																		 'redirectOnfailure' =>JURI::base()."?option=com_user&view=login" ,//JFactory::getURI()->toString(),
 																		 'skipAJAXVerification' => false,
 																		 //'onCaptchaFailSetVars' =>'view=users&task='
 																		 'isVertical' =>  true,
 																		) ;
-				$this->enabledForms['Core Joomla login Component Form'] = array('requestVars' => 'option=com_users&view=login',
-																		 'formaction_regExp' => $this->createFormActionRegexp('task=user.login'),//since the form has no id or name use action regexp here
+				//<input type="submit" value="Login" class="button" name="Submit">
+				$this->enabledForms['Core Joomla login Component Form'] = array('requestVars' => 'option=com_user&view=login',
+																		 'formId' =>"com-form-login",
 																		 //'no-id-form-ref-field' => 'task=user.login',//'task=search',//
-																		 'ignore_condition' => 'task=user.logout',
-																		 'tagToPlaceCaptchaBefore' =>'<button type="submit" class="button">',
-																		 'verifyOnVars' =>'option=com_users&task=user.login',
-																		 'redirectOnfailure' =>JURI::base()."index.php/login" ,//JFactory::getURI()->toString(),
+																		 'ignore_condition' => 'task=logout',
+																		 'tagToPlaceCaptchaBefore' =>'<input type="submit"',
+																		 'verifyOnVars' =>'option=com_user&task=login',
+																		 'redirectOnfailure' =>JURI::base()."?option=com_user&view=login" ,//JFactory::getURI()->toString(),
 																		 'skipAJAXVerification' => false,
 																		 //'onCaptchaFailSetVars' =>'view=users&task='
 																		 'isVertical' =>  false,
 																		) ;
 			}
-
 ?>
